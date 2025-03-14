8731679aec934a6aeefd0dfd704f1b38f56b6cdc
Dipesh Gaikar <dipeshgaikar23@gmail.com>
2025-03-14 17:46:59 +0530

    added the threejs component but doesnt work

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import HowItWorks from './components/HowItWorks';
 import Testimonials from './components/Testimonials';
 import CTASection from './components/CTAsection';
 import Footer from './components/Footer';
+import ThreeJSBackground from './components/ThreeJSBackground';
 
 const App = () => {
   return (
     <div>
+      <ThreeJSBackground/>
       <Header />
       <Hero />
       <FeatureCards />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section className="d-flex align-items-center justify-content-center vh-100 bg-light">
+    <section className="d-flex align-items-center justify-content-center vh-100">
       <div className="text-center">
         <h2 className="display-4 text-success fw-bold" >Connecting Natural Farmers with Conscious Consumers</h2>
         <p className="lead text-muted">HarvestTrace is a transparent marketplace that verifies authentic natural farming practices and empowers consumers to make informed choices about their food.</p>
